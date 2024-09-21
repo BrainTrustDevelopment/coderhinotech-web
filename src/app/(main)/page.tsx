c750062be45c93c4a760a82f5d41382385cf3271
Tariq Hook <tariq.hook@gmail.com>
2024-09-21 09:57:26 -0400

    updates

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,8 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="max-w-8xl mx-auto px-4 sm:px-6 md:px-8">
 
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
@@ -13,7 +12,6 @@ export default function Home() {
         </ol>
 
 
-      </main>
     </div>
   );
 }
